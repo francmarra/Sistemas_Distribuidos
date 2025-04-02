37e3c0b7722cfe42f3ef17ed4c7d777a269eeb75
Tiago Costa <tiagojose262004@gmail.com>
2025-04-02 15:04:06 +0100

    teste

--- WAVY/obj/Debug/net9.0/WAVY.AssemblyInfo.cs
+++ WAVY/obj/Debug/net9.0/WAVY.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAVY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c652a147657481b648a4ea5cff9d25cf714cb091")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9dfa809b799140b321ca23488055a1e4b43599d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAVY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAVY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
